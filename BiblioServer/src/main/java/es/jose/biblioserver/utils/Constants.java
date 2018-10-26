c26204d5be479c230b1ea5518a24c02aada10c2f
José <jbcastillonmolina@gmail.com>
2018-10-26 19:16:37 +0200

    Dependencias iniciales

---
+++ BiblioServer/src/main/java/es/jose/biblioserver/utils/Constants.java
@@ -0,0 +1,11 @@
+package es.jose.biblioserver.utils;
+
+/**
+ * Constantes comunes a toda la aplicación
+ * 
+ * @since 26-10-2018
+ * @author José B. Castillón Molina
+ */
+public class Constants {
+    
+}
